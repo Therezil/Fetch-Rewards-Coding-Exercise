d4b88d00fdf558d0cbe7caffbcaf8523a0338707
Therezil <jpaquette60@gmail.com>
2021-08-23 21:20:19 -0500

    Transaction can be added

--- FetchRewards.js
+++ FetchRewards.js
@@ -1,3 +1,20 @@
+const transactions = [];
+
 function addTransaction() {
-    document.getElementById("test").innerHTML = "11";
+    let userText = document.getElementById("userInput1").value;
+    transactions.push(userText)
+
+    if (userText == "") {
+        document.getElementById("output1").innerHTML = "Invalid Transaction"
+    } else {
+        document.getElementById("output1").innerHTML = "Transaction Added!"
+    }
+}
+
+function spendPoints() {
+    document.getElementById("output2").innerHTML = "11";
+}
+
+function returnBalances() {
+    document.getElementById("output3").innerHTML = "11";
 }
